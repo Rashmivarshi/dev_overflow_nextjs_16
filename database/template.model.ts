f2ea10b495b645a3295b336d75bf73634a63af45
Rashmi <rashmimaniampare@gmail.com>
2026-03-06 16:24:16 +0530

    Create answermodel

---
+++ database/template.model.ts
@@ -0,0 +1,7 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IModel {}
+const ModelSchema = new Schema<IModel>({});
+
+const Model = models?.Model || model<IModel>("Model", ModelSchema);
+export default Model;
