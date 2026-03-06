4114da41781c4ebcd47d4d4e9ecb686aefaf1f54
Rashmi <rashmimaniampare@gmail.com>
2026-03-06 17:11:21 +0530

    Create votemodel

--- database/template.model.ts
+++ database/template.model.ts
@@ -1,7 +1,7 @@
 import { model, models, Schema, Types } from "mongoose";
 
 export interface IModel {}
-const ModelSchema = new Schema<IModel>({});
+const ModelSchema = new Schema<IModel>({}, { timestamps: true });
 
 const Model = models?.Model || model<IModel>("Model", ModelSchema);
 export default Model;
