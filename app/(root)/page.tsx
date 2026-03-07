9dacc3275a8d4dedc42fab93c70d9ca3362b1963
Rashmi <rashmimaniampare@gmail.com>
2026-03-07 14:01:53 +0530

    Implement logger

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,8 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
 import Link from "next/link";
 
 const questions = [
@@ -60,11 +62,22 @@ const questions = [
     createdAt: new Date("2021-09-01"),
   },
 ];
+const test = async () => {
+  try {
+    throw new ValidationError({
+      title: ["Required"],
+      tags: ['"JavaScript" is not a valid tag.'],
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 const Home = async ({ searchParams }: SearchParams) => {
+  await test();
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
