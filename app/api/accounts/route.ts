a425c8ada9d1fc8497176e93b158a8596dbaf291
Rashmi <rashmimaniampare@gmail.com>
2026-03-11 14:23:36 +0530

    Implement create and read Account Route

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,40 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const accounts = await Account.find({});
+    return NextResponse.json({ sucess: true, data: accounts }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const validateData = AccountSchema.parse(body);
+    const existingUser = await Account.findOne({
+      provider: validateData.provider,
+      providerAccountId: validateData.providerAccountId,
+    });
+    if (existingUser)
+      throw new ForbiddenError("Account with this provider already exists");
+
+    const newAccount = await Account.create(validateData);
+
+    return NextResponse.json(
+      { sucess: true, data: newAccount },
+      { status: 201 },
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
