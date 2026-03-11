b737727e1e1207c8c151189fc4d6da3c69bc257b
Rashmi <rashmimaniampare@gmail.com>
2026-03-11 13:43:34 +0530

    implement user by mail

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,25 @@
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const { email } = await request.json();
+    const validateData = UserSchema.partial().safeParse({ email });
+    if (!validateData.success) {
+      throw new ValidationError(z.flattenError(validateData.error).fieldErrors);
+    }
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
