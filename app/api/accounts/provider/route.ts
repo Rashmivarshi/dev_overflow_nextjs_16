0712696261fdc4d3570ce9213c6a2f6e061c7c01
Rashmi <rashmimaniampare@gmail.com>
2026-03-11 16:17:05 +0530

    implement get account by provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,27 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const { providerAccountId } = await request.json();
+    const validateData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+    if (!validateData.success) {
+      throw new ValidationError(z.flattenError(validateData.error).fieldErrors);
+    }
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
