e196af47bd82daba493a43a4549312bae27f1991
Rashmi <rashmimaniampare@gmail.com>
2026-04-02 20:52:24 +0530

    implement render ui state

---
+++ components/DataRender.tsx
@@ -0,0 +1,113 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { DEFAULT_EMPTY, DEFAULT_ERROR } from "@/constants/states";
+
+interface Props<T> {
+  success: boolean;
+  error?: { message: string; details?: Record<string, string[]> };
+  data: T[] | null | undefined;
+  empty: {
+    title: string;
+    message: string;
+    button?: {
+      text: string;
+      href: string;
+    };
+  };
+  render: (data: T[]) => React.ReactNode;
+}
+
+interface StateSkeletonProps {
+  image: {
+    light: string;
+    dark: string;
+    alt: string;
+  };
+  title: string;
+  message: string;
+  button?: {
+    text: string;
+    href: string;
+  };
+}
+
+const StateSkeleton = ({
+  image,
+  title,
+  message,
+  button,
+}: StateSkeletonProps) => (
+  <div>
+    <>
+      <Image
+        src={image.dark}
+        alt={image.alt}
+        width={270}
+        height={200}
+        className="hidden object-contain dark:block"
+      />
+      <Image
+        src={image.light}
+        alt={image.alt}
+        width={270}
+        height={200}
+        className="block object-contain dark:hidden"
+      />
+    </>
+    <h2 className="h2-bold text-dark200_light900 mt-8">{title}</h2>
+    <p className="body-regular text-dark500_light700 my-3.5 max-w-md text-center">
+      {message}
+    </p>
+    {button && (
+      <Link href={button.href}>
+        <Button className="paragraph-regular mt-5 min-h-[46px] rounded-lg bg-primary-500 px-4 py-3 text-light-900 hover:bg-primary-500">
+          {button.text}
+        </Button>
+      </Link>
+    )}
+  </div>
+);
+const DataRender = <T,>({
+  success,
+  error,
+  data,
+  empty = DEFAULT_EMPTY,
+  render,
+}: Props<T>) => {
+  if (!success) {
+    return (
+      <StateSkeleton
+        image={{
+          light: "/public/images/light-error.png",
+          dark: "/public/images/dark-error.png",
+          alt: "Error illustration",
+        }}
+        title={error?.message || DEFAULT_ERROR.title}
+        message={
+          error?.details
+            ? JSON.stringify(error.details, null, 2)
+            : DEFAULT_ERROR.message
+        }
+        button={DEFAULT_ERROR.button}
+      />
+    );
+  }
+  if (!data || data.length == 0)
+    return (
+      <StateSkeleton
+        image={{
+          light: "/public/images/light-illustration.png.png",
+          dark: "/public/images/dark-illustration.png.png",
+          alt: "empty illustration",
+        }}
+        title={empty.title}
+        message={empty.message}
+        button={empty.button}
+      />
+    );
+
+  return <div>{render(data)}</div>;
+};
+
+export default DataRender;
