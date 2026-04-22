f5f79fb4a339f65a45a3146499baf233672446be
Rashmi B <rashmimaniampare@gmail.com>
2026-04-22 11:09:45 +0530

    Implement question details view #1

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -8,6 +8,7 @@ import { formatNumber, timeAgo } from "@/lib/utils";
 
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import View from "../View";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -20,6 +21,7 @@ const QuestionDetails = async ({ params }: RouteParams) => {
 
   return (
     <>
+      <View questionId={id} />
       <div className=" flex-start w-full flex-col">
         <div className="flex flex-col-reverse w-full justify-between ">
           <div className="flex items-center justify-start gap-1">
--- types/action.d.ts
+++ types/action.d.ts
@@ -28,3 +28,7 @@ interface GetQuestionParams {
 interface GetTagQuestionParams extends Omit<PaginatedSearchParams, "filter"> {
   tagId: string;
 }
+
+interface IncrementViewParams {
+  questionId: string;
+}
