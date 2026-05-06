0ed69bdc0b9d1fceac813ae6c10906d317564974
Rashmi B <rashmimaniampare@gmail.com>
2026-05-06 18:21:37 +0530

    Implement pagination with adjust pageSize

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -20,7 +20,7 @@ const Home = async ({ searchParams }: SearchParams) => {
   const { page, pageSize, query, filter } = await searchParams;
   const { success, data, error } = await getQuestions({
     page: Number(page) || 1,
-    pageSize: Number(pageSize) || 2,
+    pageSize: Number(pageSize) || 10,
     query: query || "",
     filter: filter || "",
   });
