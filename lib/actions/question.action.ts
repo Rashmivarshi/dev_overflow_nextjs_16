a08b0b602bafcabe922664fa02b3e26b1be4d398
Rashmi B <rashmimaniampare@gmail.com>
2026-05-15 11:29:54 +0530

    Implement cache to server action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -24,6 +24,7 @@ import { Answer, Collection, Interaction, Vote } from "@/database";
 import { after } from "next/server";
 import { createInteraction } from "./interaction.action";
 import { auth } from "@/auth";
+import { cache } from "react";
 
 export async function createQuestion(
   params: CreateQuestionParams,
@@ -205,8 +206,7 @@ export async function editQuestion(
     await session.endSession();
   }
 }
-
-export async function getQuestion(
+export const getQuestion = cache(async function getQuestion(
   params: GetQuestionParams,
 ): Promise<ActionResponse<Question>> {
   const validationResult = await action({
@@ -234,7 +234,7 @@ export async function getQuestion(
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
-}
+});
 
 export async function getRecommendedQuestions({
   userId,
